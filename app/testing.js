a4f02bc93bd019bd2e88ab2721887e481fcbe0c0
Jose Albeldo <rayracing@gmail.com>
2023-05-02 21:58:55 +0200

    Diferent files to find a good way

---
+++ app/testing.js
@@ -0,0 +1,13 @@
+const strictEquals = require("./index");
+describe("Given a sum function", ()=> {
+  describe("When it receives --- and ---",()=>{
+    test("Then it should returns 5", ()=>{
+      const valueA = 1;
+      const ValueB = 1;
+      const resultTotal = --;
+
+      //ACT
+      const total = 
+    })
+  })
+})
